e4977735e0e104bb689b44a91e6011ebc287d8ab
Michał Gałka <michalgalka84@gmail.com>
2024-03-06 13:04:01 +0100

    cleanup

--- main.py
+++ main.py
@@ -22,17 +22,13 @@ def find_indexes(word, letter):
 for _ in word:
     user_word.append("_")
 
-def letter_not_found(no_of_tries):
-    print("No such letter")
-    no_of_tries -= 1
-    print("Number of tries left:", no_of_tries)
-    if (no_of_tries == 0):
-        print("Game Over")
-        sys.exit(0)
-
 
 def noHit(found_letter_indexes):
     return len(found_letter_indexes) == 0
+
+
+def no_tries_left():
+    return no_of_tries == 0
 
 
 # Main app flow
@@ -45,7 +41,7 @@ while True:
         print("No such letter")
         no_of_tries -= 1
         print("Number of tries left:", no_of_tries)
-        if (no_of_tries == 0):
+        if no_tries_left():
             print("Game Over")
             sys.exit(0)
     else:
