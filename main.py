9a3be0cf1fc20f1f3bb34fd7a04b54559d70233e
Michał Gałka <michalgalka84@gmail.com>
2024-03-06 11:57:57 +0100

    add happy path flow

--- main.py
+++ main.py
@@ -28,9 +28,16 @@ while True:
     if len(indexes) == 0:
         print("No such letter")
         no_of_tries -= 1
+        print("No of tries left:", no_of_tries)
         if(no_of_tries == 0):
             print("Game Over")
             sys.exit(0)
-
+    else:
+        for index in indexes:
+            user_word[index] = letter
+        if("".join(user_word) == word):
+            print("That's correct!: ", word)
+            sys.exit(0)
 
     print("Used letters:", used_letters)
+    print("".join(user_word))
