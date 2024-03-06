7eedb323b54900bc41a830a96def9d92873d70d3
Michał Gałka <michalgalka84@gmail.com>
2024-03-06 11:49:43 +0100

    add check no of tries

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
+import sys
 
 no_of_tries = 5
-expected_word = "Guess me"
+word = "Guess me"
 
-user_letters = []
 used_letters = []
+user_word = []
 
 
-for _ in expected_word:
-    user_letters.append("_")
+###
+def find_indexes(word, letter):
+    indexes = []
+    for index, letter_in_word, in enumerate(word):
+        if letter == letter_in_word:
+            indexes.append(index)
+
+    return indexes
+
+
+
+for _ in word:
+    user_word.append("_")
 
 while True:
     letter = input("Enter a letter: ")
     used_letters.append(letter)
+    indexes = find_indexes(word, letter)
+    if len(indexes) == 0:
+        print("No such letter")
+        no_of_tries -= 1
+        if(no_of_tries == 0):
+            print("Game Over")
+            sys.exit(0)
 
-    print(user_letters)
+
+    print("Used letters:", used_letters)
